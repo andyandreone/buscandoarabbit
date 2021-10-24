812571586af8eac3cadab79890e09943603f22d0
andyandreone <andy1.-@hotmail.com>
2021-10-24 17:30:05 -0300

    geolocalitation

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var watchID = navigator.geolocation.watchPosition(function(position) {
     console.log(position.coords.latitude, position.coords.longitude);
+    let latDom = document.getElementById("latitud")
+    let longDom = document.getElementById("longitud")
+    latDom.innerHTML = position.coords.latitude
+    longDom.innerHTML = position.coords.longitude
   });
+
+ 
