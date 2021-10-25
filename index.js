6fa28e8f2f71457ee5d47ad639ee5464e7297d6e
andyandreone <andy1.-@hotmail.com>
2021-10-25 15:22:16 -0300

    vuelta a casa

--- index.js
+++ index.js
@@ -24,17 +24,27 @@ var watchID = navigator.geolocation.watchPosition(function(position) {
     // let longMaxAndy = -58.59620
 
     let dif = 0.00020
-    let latSub = -34.61678
-    let longsub = -58.41062
+    let lat1 = -34.62761
+    let long1 = -58.41788
 
-    let latCar = -34.61693
-    let longCar = -58.40987
+    let lat2 = -34.63360
+    let long2 = -58.44331
 
-    let latPed = -34.61856
-    let longPed = -58.41011
+    let lat3 = -34.64336
+    let long3 = -58.46189
 
-    let latTrau = -34.61838
-    let longTrau = -58.41065
+    let lat4 = -34.63671
+    let long4 = -58.49585
+
+    let lat5 = -34.63545
+    let long5 = -58.52982
+
+    let lat6 = -34.65162
+    let long6 = -58.53308
+
+    let lat7 = -34.66094
+    let long7 = -58.54450
+ 
     
    
 
@@ -43,34 +53,58 @@ var watchID = navigator.geolocation.watchPosition(function(position) {
     //     textoPista.innerText = "DESBLOQUEADA!"
     //     button.disabled = false
     // }
-    if((lat>latSub-dif&&lat<latsub+dif)&&(long>longsub-dif)&&(long<longsub+dif)){
+    if((lat>lat1-dif&&lat<lat1+dif)&&(long>long1-dif)&&(long<long1+dif)){
         let textoPista = document.getElementById("textPista1")
         textoPista.innerText = "DESBLOQUEADA!"
         let btn1 = document.getElementById("btn1")
-        btn.disabled = false
-       
+        btn1.disabled = false
     }
+    
 
-    if((lat>latCar-dif&&lat<latCar+dif)&&(long>longCar-dif)&&(long<longCar+dif)){
+    if((lat>lat2-dif&&lat<lat2+dif)&&(long>long2-dif)&&(long<long2+dif)){
         let textoPista = document.getElementById("textPista2")
         textoPista.innerText = "DESBLOQUEADA!"
         let btn2 = document.getElementById("btn2")
         btn2.disabled = false
     }
 
-    if((lat>latPed-dif&&lat<latPed+dif)&&(long>longPed-dif)&&(long<longPed+dif)){
+    if((lat>lat3-dif&&lat<lat3+dif)&&(long>long3-dif)&&(long<long3+dif)){
         let textoPista = document.getElementById("textPista3")
         textoPista.innerText = "DESBLOQUEADA!"
         let btn3 = document.getElementById("btn3")
         btn3.disabled = false
     }
 
-    if((lat>latTrau-dif&&lat<latTrau+dif)&&(long>longTrau-dif)&&(long<longTrau+dif)){
+    if((lat>lat4-dif&&lat<lat4+dif)&&(long>long4-dif)&&(long<long4+dif)){
         let textoPista = document.getElementById("textPista4")
         textoPista.innerText = "DESBLOQUEADA!"
         let btn4 = document.getElementById("btn4")
         btn4.disabled = false
     }
+
+    if((lat>lat5-dif&&lat<lat5+dif)&&(long>long5-dif)&&(long<long5+dif)){
+        let textoPista = document.getElementById("textPista5")
+        textoPista.innerText = "DESBLOQUEADA!"
+        let btn5 = document.getElementById("btn5")
+        btn5.disabled = false
+    }
+
+
+    if((lat>lat6-dif&&lat<lat6+dif)&&(long>long6-dif)&&(long<long6+dif)){
+        let textoPista = document.getElementById("textPista6")
+        textoPista.innerText = "DESBLOQUEADA!"
+        let btn6 = document.getElementById("btn6")
+        btn6.disabled = false
+    }
+
+
+    if((lat>lat7-dif&&lat<lat7+dif)&&(long>long7-dif)&&(long<long7+dif)){
+        let textoPista = document.getElementById("textPista7")
+        textoPista.innerText = "DESBLOQUEADA!"
+        let btn7 = document.getElementById("btn7")
+        btn7.disabled = false
+    }
+
 
 });
 
@@ -88,4 +122,16 @@ function mostrar3(){
 
 function mostrar4(){
     alert("Pista 4 Desbloqueada")
+}
+
+function mostrar5(){
+    alert("Pista 5 Desbloqueada")
+}
+
+function mostrar6(){
+    alert("Pista 6 Desbloqueada")
+}
+
+function mostrar7(){
+    alert("Pista 7 Desbloqueada")
 }
